37e9c9f816fc8f82adb61607e39c3502dfa4ca8f
Julianne Gonski <jtgonski@Juliannes-MacBook-Pro.local>
2017-07-11 18:27:04 -0400

    changed a few things with mark's help

--- assets/javascript/psychic-game.js
+++ assets/javascript/psychic-game.js
@@ -1,10 +1,14 @@
+
+//Starting variables 
 var wins = 0;
 var losses = 0;
 var guessesLeft = 10;
 var charSet = "abcdefghijklmnopqrstuvwxyz";
 var wrongLetters = " ";
 var compLetter = charSet.charAt(Math.floor(Math.random() * charSet.length));
-console.log(compLetter);
+	//check computer's letter
+	console.log(compLetter);
+	
 var userGuess; 
 
 //reset function to keep game going after the first round 
@@ -21,11 +25,9 @@ var beginAgain = function() {
 }
 
 //determines if user has picked a letter key
-//was unable to solve how to implement such a function before assignment was due... 
-var isLetterKey = function() { 
-	userGuess === "a" || "b" || "c" || "d" || "e" || "f" || "g" || "h" || "i" || "j" || "k" 
-	|| "l" || "m" || "n" || "o" || "p" || "q" || "s" || "t" || "u" || "v" || "w" || "x" || "y" 
-	|| "z" ; 
+var isLetterKey = function(userGuess) { 
+	return userGuess >=  "a" && userGuess <= "z"; 
+
 }
 
 //let the games begin! Users picks a key: 
@@ -33,30 +35,37 @@ document.onkeyup = function(event) {
 
 		var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
-		guessesLeft = guessesLeft - 1; 
-		document.getElementById("guessesLeft").innerHTML = guessesLeft; 
-	
-		if (guessesLeft > 0) {
+	if (isLetterKey(userGuess)) {
 
-			wrongLetters += userGuess + ", "; 
-			document.getElementById("wrongLetters").innerHTML = wrongLetters;
+			guessesLeft = guessesLeft - 1; 
+			document.getElementById("guessesLeft").innerHTML = guessesLeft; 
+		
+			if (guessesLeft > 0) {
+
+				wrongLetters += userGuess + ", "; 
+				document.getElementById("wrongLetters").innerHTML = wrongLetters;
 
 
-			if (userGuess === compLetter) {
-				alert("you won! I was thinking of '" + compLetter + "'");
-				wins = wins + 1; 
-				document.getElementById("wins").innerHTML = wins;
-				beginAgain();
+				if (userGuess === compLetter) {
+					alert("you won! I was thinking of '" + compLetter + "'");
+					wins = wins + 1; 
+					document.getElementById("wins").innerHTML = wins;
+					beginAgain();
+				}
+
 			}
 
+			else {
+				losses = losses + 1; 
+				document.getElementById("losses").innerHTML = losses;
+				alert("you lost this round! I was thinking of '" + compLetter + "'"); 
+				alert("try again! Pick another letter to begin"); 
+				beginAgain(); 
+			}
 		}
 
 		else {
-			losses = losses + 1; 
-			document.getElementById("losses").innerHTML = losses;
-			alert("you lost this round! I was thinking of '" + compLetter + "'"); 
-			alert("try again! Pick another letter to begin"); 
-			beginAgain(); 
+			alert("Please pick a letter!");
 		}
 	}
 
